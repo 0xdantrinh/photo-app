4a3738ba135958793b443d3010fbed7ffa51363b
Daniel Trinh <dantrinh@seas.upenn.edu>
2018-11-27 18:03:28 -0800

    adding stripe js handler

---
+++ app/models/payment.rb
@@ -0,0 +1,24 @@
+class Payment < ActiveRecord::Base
+  attr_accessor :card_number, :card_cvv, :card_expires_month, :card_expires_year
+  belongs_to :user
+
+  def self.month_options
+    Date::MONTHNAMES.compact.each_with_index.map { |name, i| ["#{i+1} - #{name}", i+1] }
+  end
+
+  def self.year_options
+    (Date.today.year..(Date.today.year + 10)).to_a
+  end
+
+  def process_payment
+    debugger
+    customer = Stripe::Customer.create(card: token, email: email)
+
+    Stripe::Charge.create customer: customer.id,
+                          amount: 1000,
+                          description: 'Premium',
+                          currency: 'usd',
+                          source: customer.default_source
+  end
+
+end
